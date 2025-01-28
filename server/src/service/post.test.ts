759e93fba1df6e3761f4059934c2ababc617fd9b
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2025-01-28 15:34:23 +0100

    create post service tests

---
+++ server/src/service/post.test.ts
@@ -0,0 +1,25 @@
+import { PostService } from "./post";
+import { UserService } from "./user";
+
+test("If a post is added to the list then it should be in the list", async () => {
+  const userService = new UserService();
+
+  const user = await userService.createUser(
+    "zeb",
+    "bjo",
+    "test@email.com",
+    "testpass"
+  );
+  const text = "Test text";
+  const author = user;
+  const title = "Test title";
+  const postService = new PostService();
+  await postService.createPost(text, author, title);
+  const posts = await postService.getPosts();
+  expect(
+    posts.some(
+      (post) =>
+        post.text === text && post.author === author && post.title === title
+    )
+  ).toBeTruthy();
+});
