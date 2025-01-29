00d4998cf35cc17adeada38e6b766e1dee30c62e
SaraBatHedberg <aras.hedberg@gmail.com>
2025-01-29 10:43:51 +0100

    create test for user router
    created a test for the post that returns the current user. might need to be updated

---
+++ server/src/router/user.test.ts
@@ -0,0 +1,9 @@
+import * as SuperTest from "supertest";
+import { app } from "../start";
+const request = SuperTest.default(app);
+
+test("Post /user - should return the current user", async () => {
+    const res = await request.post("/user");
+
+    expect(res.statusCode).toBe(200);
+});
