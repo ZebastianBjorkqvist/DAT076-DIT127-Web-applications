a1efaec18d7c85c08515fc6be9e03a9324afc4ab
andreasmagn <andreas@magn.se>
2025-02-03 13:52:27 +0100

    updated start.test
    user-object instead of string as author

--- server/src/start.test.ts
+++ server/src/start.test.ts
@@ -1,20 +1,29 @@
 import * as SuperTest from "supertest";
 import { app } from "./start";
 import { Post } from "./model/post";
+import { User } from "./model/user";
 
 const request = SuperTest.default(app);
 
 test("End-to-end test", async () => {
+  const authorObject : User = {
+    id: Date.now(),
+    firstName: "Test",
+    lastName: "User",
+    email: "test.user@gmail.com",
+    password: "testpass",
+  }
+
   const testCreatePostInput = {
     text: "This is a test post",
-    author: "testUserId",
+    author: authorObject,
     title: "Test title"
   }
 
   const res1 = await request.post("/post").send(testCreatePostInput);
   expect(res1.statusCode).toEqual(201);
   expect(res1.body.text).toEqual(testCreatePostInput.text);
-  expect(res1.body.author).toEqual({});
+  expect(res1.body.author).toEqual(authorObject);
   expect(res1.body.title).toEqual(testCreatePostInput.title);
 
   const res2 = await request.get("/post");
