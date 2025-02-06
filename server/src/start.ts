7657b7a351db041fe41318a57e7983cd7cd14c72
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2025-02-06 10:06:28 +0100

    Initial backend setup for lab3

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { postRouter } from "./router/post";
 import { userRouter } from "./router/user";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/post", postRouter);
 app.use("/user", userRouter);
