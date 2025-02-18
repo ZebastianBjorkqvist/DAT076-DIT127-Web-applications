35579739543ee426370043cffd99f16c1bb6bc6e
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2025-02-18 14:03:13 +0100

    Add styling for subtitle

--- client/src/components/loginHeader.tsx
+++ client/src/components/loginHeader.tsx
@@ -1,11 +1,12 @@
 import logo from "../assets/Logo.svg";
+import "../styles/index.css";
 function Header() {
   return (
     <header className="py-3 standard-font">
       <div className="container text-center">
         <img src={logo} alt="Chatter Logo" className="img" width="500" />
         <h1 className="header-text">Welcome to Chatter!</h1>
-        <p className="header-text">Sign in or create an account</p>
+        <p className="subtitle">Sign in or create an account</p>
       </div>
     </header>
   );
