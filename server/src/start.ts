ba11619ee7fbe26eaab532702687abcf5a44b14c
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2025-02-18 18:07:53 +0100

    Setup for lab 4

--- server/src/start.ts
+++ server/src/start.ts
@@ -2,10 +2,29 @@ import express from "express";
 import { postRouter } from "./router/post";
 import { userRouter } from "./router/user";
 import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
 
 export const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+dotenv.config();
+if (!process.env.SESSION_SECRET) {
+  console.log("Could not find SESSION_SECRET in .env file");
+  process.exit();
+}
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use("/post", postRouter);
 app.use("/user", userRouter);
