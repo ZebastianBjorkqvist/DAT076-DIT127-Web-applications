af3d2963cbc95a1ba42d206ca1f82278ef007a66
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2025-02-18 15:13:37 +0100

    add props to feedcard

--- client/src/components/feedCard.tsx
+++ client/src/components/feedCard.tsx
@@ -4,12 +4,12 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-function feedCard() {
+function feedCard(props: any) {
   return (
     <div className="post-card">
       <Row className="post-card-header">
         <Col>
-            <header className="header-text">Post name </header>
+          <header className="header-text">{props.title} </header>
         </Col>
         <Col>
           <img src={UserIcon} alt="Chatter Logo" className="img" width="50" />
@@ -17,9 +17,7 @@ function feedCard() {
       </Row>
 
       <Row className="post-card-text">
-       <text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at pharetra magna. Vivamus tristique lectus sit amet nibh hendrerit scelerisque. Curabitur at semper turpis, non tempus purus. Etiam at ligula in purus varius luctus ut sed nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec fringilla dui at nibh vestibulum, at porta felis dictum. Donec leo dolor, auctor ac mattis vel, eleifend a nisl. Donec tristique, velit eget posuere sodales, nulla justo mattis dui, eu iaculis urna justo ac diam. Morbi nec rhoncus nunc. 
-        </text>
+        <text>{props.description}</text>
       </Row>
     </div>
   );
