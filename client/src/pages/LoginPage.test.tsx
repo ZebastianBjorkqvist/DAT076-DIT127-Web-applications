c7d4ad9589f17b8eabca096bbcc54ba4a2181be3
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2025-02-20 10:51:26 +0100

    Move testing to page instead of component

---
+++ client/src/pages/LoginPage.test.tsx
@@ -0,0 +1,65 @@
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import LoginPage from "./LoginPage";
+import { MemoryRouter } from "react-router-dom";
+import axios from "axios";
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe("LoginPage", () => {
+  beforeEach(() => {});
+
+  test("renders form", () => {
+    render(
+      <MemoryRouter>
+        <LoginPage />
+      </MemoryRouter>
+    );
+    const loginButton = screen.getByRole("button", { name: "Login" });
+    const usernameField = screen.getByRole("textbox", { name: "" });
+    const pwdField = screen.getByLabelText("Password");
+    const createButton = screen.getByRole("button", { name: "Create user" });
+
+    expect(createButton).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+    expect(usernameField).toBeInTheDocument();
+    expect(pwdField).toBeInTheDocument();
+  });
+
+  it("sends correct login data to backend", async () => {
+    render(
+      <MemoryRouter>
+        <LoginPage />
+      </MemoryRouter>
+    );
+    const loginButton = screen.getByRole("button", { name: "Login" });
+    const usernameField = screen.getByRole("textbox", { name: "" });
+    const pwdField = screen.getByLabelText("Password");
+    await act(async () => {
+      fireEvent.change(await usernameField, { target: { value: "zeb" } });
+      fireEvent.change(await pwdField, { target: { value: "123" } });
+      fireEvent.click(await loginButton);
+    });
+
+    await waitFor(() => {
+      expect(mockedAxios.post).toHaveBeenCalledWith(
+        "http://localhost:8080/user/login",
+        {
+          username: "zeb",
+          password: "123",
+        }
+      );
+    });
+  });
+
+  it("logs rejects an incorrect password", () => {});
+
+  it("logs rejects an invalid user", () => {});
+
+  it("logs rejects an incorrect password and an incorrect user in the same way", () => {});
+});
