55f2d83c847df73b46eb2d4de0a6a274ed873011
Marko Horvat <marko.horvat077@gmail.com>
2025-02-25 13:46:04 +0100

    User is redirected to feed if logged in while trying to access login page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -3,8 +3,23 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../styles/App.css";
 import Login from "../components/loginForm.tsx";
 import Header from "../components/loginHeader.tsx";
+import { checkAuth } from "../api";
+import { useEffect, useState } from "react";
+import { redirect, useNavigate } from "react-router";
 
-function LoginPage() {
+
+function LoginPage() {  
+  const navigate = useNavigate();
+
+  useEffect(() => {
+     checkAuth()
+      .then((isAuthenticated) => {
+        if (isAuthenticated) {
+          navigate("/feed")
+        }
+      })
+  })
+
   return (
     <>
       <Container className="d-flex justify-content-center align-items-center vh-100">
